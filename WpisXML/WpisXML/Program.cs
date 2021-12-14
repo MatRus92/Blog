68bb994a67362552f08858a5fad6258288f0fee1
MatRus92 <kontakt@mateuszrus.pl>
2021-12-14 19:01:57 +0100

    Przykład 1

---
+++ WpisXML/WpisXML/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WpisXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateXml();
+        }
+
+        static private void CreateXml()
+        {
+            XmlTextWriter writer = new XmlTextWriter("Osoba.xml", System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("osoba");
+            CreateNode("imie", "Mateusz", writer);
+            CreateNode("nazwisko", "Rus", writer);
+            CreateNode("zawod", "Programista", writer);
+            CreateNode("hobby", "Siatkowka", writer);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+            Console.WriteLine("Plik XML został stworzony!");
+        }
+
+        static private void CreateNode(string tag, string value, XmlTextWriter writer)
+        {
+            writer.WriteStartElement(tag);
+            writer.WriteString(value);
+            writer.WriteEndElement();
+        }
+    }
+}
